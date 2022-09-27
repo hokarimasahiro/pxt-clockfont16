86c0f80a1ba0f39a7df9ca08e7c11bb4d05ccf02
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2022-09-28 08:12:30 +0900

    更新 pxt.json, test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
-// テストはここに来ます。このパッケージが拡張機能として使用されるときにはコンパイルされません。
+basic.forever(function() {
+    clockFont.clearColumn(0, 16);
+    clockFont.displayColon(11)
+
+    for(let i=0;i<10;i++){
+        clockFont.displayNumber(0, i, 8);
+        basic.pause(1000);
+    }
+    for (let i = 0; i < 10; i++) {
+        clockFont.displayNumber(0, i, 9);
+        basic.pause(1000);
+    }
+})
