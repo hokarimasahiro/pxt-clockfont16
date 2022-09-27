7cae1b8c4571f4039cdca147b10f91cce12776a3
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2022-09-28 08:43:12 +0900

    更新 test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,14 @@
-basic.forever(function() {
-    clockFont.clearColumn(0, 16);
-    clockFont.displayColon(11)
+let hour:number=9;
+let minute:number=10;
+let second:number=30;
+clockFont.setBrightness(32);
+clockFont.setColor(0xffff00);
 
-    for(let i=0;i<10;i++){
-        clockFont.displayNumber(0, i, 8);
-        basic.pause(1000);
-    }
-    for (let i = 0; i < 10; i++) {
-        clockFont.displayNumber(0, i, 9);
-        basic.pause(1000);
-    }
+basic.forever(function() {
+    clockFont.showClock(hour, minute, second);
+    second++;
+    if (second >= 60){second = 0;minute++}
+    if (minute >= 60){minute = 0;hour++}
+    if (hour >= 24){hour = 0}
+    basic.pause(100)
 })
